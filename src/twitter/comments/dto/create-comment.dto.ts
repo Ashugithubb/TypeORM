b073331f56352e32a078f872ac675e9b320f9014
Ashutosh Kumar <itsray650@gmail.com>
2025-06-25 18:11:09 +0530

    folder structure

--- src/twitter/comments/dto/create-comment.dto.ts
+++ src/twitter/comments/dto/create-comment.dto.ts
@@ -1,1 +1,13 @@
-export class CreateCommentDto {}
+import { IsNotEmpty, IsString, IsUUID, IsInt } from 'class-validator';
+
+export class CreateCommentDto {
+  @IsUUID()
+  user_id: string;
+
+  @IsInt()
+  tweet_id: number;
+
+  @IsString()
+  @IsNotEmpty()
+  content: string;
+}
--- src/twitter/comments/entities/comment.entity.ts
+++ src/twitter/comments/entities/comment.entity.ts
@@ -1,1 +1,38 @@
-export class Comment {}
+import { Tweet } from 'src/twitter/entity/tweeets.entity';
+import { User } from 'src/twitter/entity/user.entity';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+
+
+@Entity('comments')
+export class Comment {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  user_id: string;
+
+  @Column()
+  tweet_id: number;
+
+  @Column('text')
+  content: string;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  created_at: Date;
+
+  // Relations
+  @ManyToOne(() => User, user => user, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @ManyToOne(() => Tweet, tweet => tweet, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'tweet_id' })
+  tweet: Tweet;
+}
