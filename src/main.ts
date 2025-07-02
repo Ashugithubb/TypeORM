a6cda9e15b42a9449ca83c5fd14e3bc3ac4e7125
Ashutosh Kumar <itsray650@gmail.com>
2025-07-02 18:11:20 +0530

    review

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
    app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
-  // console.log(process.env.JWT_SECRET)
+ 
 }
 bootstrap();
